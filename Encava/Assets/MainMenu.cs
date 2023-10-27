ec71526372911b768dc3cb8ee0b314155874e439
BoredFrog <wgsu300@hotmail.com>
2023-10-27 02:01:37 -0400

    mainMenuAgregado

--- Encava/Assets/MainMenu.cs
+++ Encava/Assets/MainMenu.cs
@@ -6,10 +6,22 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     // Start is called before the first frame update
+    public Animator animador;
+    public GameObject negro;
+    public musicaTransicion musica;
         
     public void EscenaJuego()
     {
-        SceneManager.LoadScene("Juego");
+        animador.SetTrigger("trigger");
+        negro.SetActive(true);
+        musica.transicion = true;
+        StartCoroutine(ChangeScene());
+    }
+    public IEnumerator ChangeScene()
+    {
+        yield return new WaitForSeconds(2.5f);
+        Debug.Log("Me cambie");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
     public void Salir()
---
+++ Encava/Assets/Scripts/musicaTransicion.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class musicaTransicion : MonoBehaviour
+{
+    public bool transicion;
+    private AudioSource musica;
+    // Start is called before the first frame update
+    void Start()
+    {
+        musica = GetComponent<AudioSource>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transicion)
+        {
+            musica.volume -= 0.8f * Time.deltaTime;
+        }
+    }
+}
